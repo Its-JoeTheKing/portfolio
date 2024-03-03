67967382aa3e7691de03f55f468fa77d347bfef0
unknown <errfig22@gmail.com>
2024-03-03 23:05:52 +0100

    added projects

--- script.js
+++ script.js
@@ -29,10 +29,23 @@ data = [
         "desc": "A website to test and try the new AI Technique named DALL E2 using API of openai.com",
         "link": "https://www.autodavinci.cf"
     },
+    {
+        "img": "./imgs/projects/6.jpg",
+        "name": "Room homepage",
+        "desc": "Responsive Home Commerce Website in HTML CSS JS",
+        "link": "https://its-joetheking.github.io/room-homepage-master/"
+    },
+    {
+        "img": "./imgs/projects/7.jpg",
+        "name": "Web News",
+        "desc": "Responsive News Homepage Website in HTML CSS",
+        "link": "https://its-joetheking.github.io/frontendmentor-news-homepage-main/"
+    },
+    
 ]
 let i = 1;
 setInterval(()=>{
-    if (i < 5) {
+    if (i < 7) {
         document.getElementById("image").src = data[i]["img"]
         document.getElementById("numb").innerText = "0"+i
         document.getElementById("title").innerText = data[i]["name"]
@@ -46,7 +59,7 @@ setInterval(()=>{
 let f = 0;
 var slideRight = () => {
     f++;
-    if (f >= 5) {
+    if (f >= 7) {
         f = 0;
     }
     document.getElementById("imageM").src = data[f]["img"];
@@ -58,7 +71,7 @@ var slideRight = () => {
 var slideLeft = () => {
     f--;
     if (f <= 0) {
-        f = 4;
+        f = 6;
     }
     document.getElementById("imageM").src = data[f]["img"];
     document.getElementById("titleM").innerText = data[f]["name"];
